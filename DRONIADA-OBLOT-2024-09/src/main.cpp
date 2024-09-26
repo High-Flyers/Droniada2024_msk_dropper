6ad87f7c10a4bf85d46d7840ee4cce65c6025753
Kacper <ks300578@student.polsl.pl>
2024-09-26 18:31:17 +0200

    final2

--- DRONIADA-OBLOT-2024-09/src/main.cpp
+++ DRONIADA-OBLOT-2024-09/src/main.cpp
@@ -9,23 +9,25 @@ bool wait = 0;
 void setup() {
     Serial.begin(9600);
     Dropper::attach();
-    Dropper::openAll();
-    Serial.println("Begin");
+    //w razie restartu - zablokuj
+    Dropper::setAll(LOCKED_SERVO_US);
+    //Serial.println("Begin");
 }
 
 void loop() {
+    //zlicza dlugosc pulsu PWM
     uint16_t _val = pulseIn(PWM_IN, HIGH);	
-    Serial.println(_val);
+    //Serial.println(_val);
+    //sprawdz czy nacisnieto guzik
     Dropper::check();
 
     if (_val > 1500 && _val < 2100 && !wait) {
         wait = !wait;
-        Dropper::dropMsk(i);
-        Serial.print("Dropped MSK: "); Serial.println(i);
+        Dropper::chooseServo(i, UNLOCKED_SERVO_US);
+        //Serial.print("Dropped MSK: "); Serial.println(i);
         i++;
         if (i >= 4)
             i = 0;
-        //delay(3000);
     }
 
     if(_val > 800 && _val < 1200 && wait)
